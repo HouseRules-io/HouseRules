0f3913b7f4943b4722d60aa59b3071d154743dc3
NickDeas <nickid0419@gmail.com>
2019-09-03 15:25:08 -0400

    Fixing admin page

--- hr/admin.py
+++ hr/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.utils.html import format_html
 
 # Register your models here.
 
@@ -7,20 +6,6 @@ from .models import Rule
 from .models import Rulebook
 from .models import House
 
-# admin.site.register(House)
-
-
-class HouseAdmin(admin.ModelAdmin):
-
-    def image_tag(self, obj):
-        return format_html('<img src="{}" />'.format(obj.qr_code.url))
-
-    image_tag.short_description = 'Image'
-
-    list_display = ['image_tag',]
-
-
-
-admin.site.register(House, HouseAdmin)
+admin.site.register(House)
 admin.site.register(Rule)
 admin.site.register(Rulebook)
