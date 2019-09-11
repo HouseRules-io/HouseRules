d433ed1075dc6928b8837c18ae28cac20611bd25
jacobvsargent <jacobvsargent@gmail.com>
2019-09-11 14:56:57 +0700

    x

--- houserules/settings.py
+++ houserules/settings.py
@@ -103,7 +103,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'HOST': 'localhost',
-            'PORT': '5432',
+            'PORT': '3306',
             'NAME': 'house-rules-db',
             'USER': 'postgres',
             'PASSWORD': 'postgres',
